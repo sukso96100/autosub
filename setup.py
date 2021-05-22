9bd4fc341309df823dc4044729bb667604010376
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-05-22 22:48:00 +0900

    Use google api client 2.5.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         ],
     },
     install_requires=[
-        'google-api-python-client>=1.4.2',
+        'google-api-python-client>=2.5.0',
         'requests>=2.25.1',
         'pysrt>=1.1.2',
         'progressbar2>=3.53.1',
